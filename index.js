bca388e83a0b227837c558d1c086b34c1d065959
Carlaamylawrence <carlaamylawrence@gmail.com>
2022-06-26 15:12:35 +0200

    final changes

--- index.js
+++ index.js
@@ -1,69 +1,126 @@
-let lists = JSON.parse(localStorage.getItem("records"))
-  ? JSON.parse(localStorage.getItem("records"))
-  : [
-      {
-        title: "Make bed",
-        date: new Date(),
-        id: 1,
-      },
-    ];
-
-function showItems(lists) {
-  document.querySelector("#displayRecords").innerHTML = "";
-  lists.forEach((record, i) => {
-    document.querySelector(
-      "#displayRecords"
-    ).innerHTML += `<li class="item"> <input type="checkbox" id="complete"> ${record.title} <button class="delete">
-                <i class="far fa-trash-alt"></i>
-            </button></li>`;
-  });
-}
-
-function addRecord() {
-  //   e.preventDefault();
-  lists.push({
-    title: document.getElementById("title").value,
-    date: new Date(),
-    id: lists.length + 1,
-  });
-  // save item
-  showItems(lists);
-  console.log(lists);
-  localStorage.setItem("records", JSON.stringify(lists));
-}
-
-function deleteRecord(id) {
-  if (id > -1) {
-    lists.splice(id, 1);
-    // After delete
-    localStorage.setItem("records", JSON.stringify(lists));
-  } else {
-    console.log("Name was not found");
-  }
-  loadDate();
-  showItems(lists);
-}
-// load data
-function loadDate() {
-  console.table(lists);
-}
-function completed(){
-lists = document.querySelector('ul');
-lists.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'li') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-}
-// add event listerner
-document.querySelector("#addRecord").addEventListener("click", addRecord);
-
-// delete record
-document.querySelector("#deleteRecord").addEventListener("click", (e) => {
+// READ
+window.onload = loadTasks;
+document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
-  let title = document.getElementById("title").value;
-  let index = lists.findIndex((item) => item.title === title);
-  console.log(index);
-  deleteRecord(index);
+  addTask();
 });
+function loadTasks() {
+  if (localStorage.getItem("tasks") == null) return;
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+  tasks.forEach((task) => {
+    const list = document.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerHTML = `<li><input type="checkbox" onclick="taskComplete(this)" class="check" ${
+      task.completed ? "checked" : ""
+    }>
+          <input type="text" value="${
+            task.task
+          }" class="task m-2 form-control ${
+      task.completed ? "completed" : ""
+    }" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+          <i class="fa fa-trash" onclick="removeTask(this)"></i></li>`;
+    list.insertBefore(li, list.children[0]);
+    console.log(tasks);
+  });
+}
+
+// CREATE
+function addTask() {
+  const task = document.querySelector("form input");
+  const list = document.querySelector("ul");
+  if (task.value === "") {
+    alert("Please add some task!");
+    return false;
+  }
+  if (document.querySelector(`input[value="${task.value}"]`)) {
+    alert("Task already exist!");
+    return false;
+  }
+  localStorage.setItem(
+    "tasks",
+    JSON.stringify([
+      ...JSON.parse(localStorage.getItem("tasks") || "[]"),
+      { task: task.value, completed: false },
+    ])
+  );
+  const li = document.createElement("li");
+  li.innerHTML = `<li>
+    <input type="checkbox" id="check" onclick="taskComplete(this)" class="check">
+      <input type="text" value="${task.value}" class="task form-control" onfocus="getCurrentTask(this)" onblur="editTask(this)">
+      <i class="fa fa-trash" onclick="removeTask(this)"></i></li>`;
+  list.insertBefore(li, list.children[0]);
+  task.value = "";
+}
+
+// CHECK IF COMPLETED
+function taskComplete(event) {
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+  tasks.forEach((task) => {
+    if (task.task === event.nextElementSibling.value) {
+      task.completed = !task.completed;
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  event.nextElementSibling.classList.toggle("completed");
+}
+
+// DELETE
+function removeTask(event) {
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+  tasks.forEach((task) => {
+    if (task.task === event.parentNode.children[1].value) {
+      tasks.splice(tasks.indexOf(task), 1);
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  event.parentElement.remove();
+}
+let currentTask = null;
+function getCurrentTask(event) {
+  currentTask = event.value;
+}
+
+// UPDATE
+function editTask(event) {
+  let tasks = Array.from(JSON.parse(localStorage.getItem("tasks")));
+  if (event.value === "") {
+    alert("Task is empty!");
+    event.value = currentTask;
+    return;
+  }
+  tasks.forEach((task) => {
+    if (task.task === event.value) {
+      alert("Task already exist!");
+      event.value = currentTask;
+      return;
+    }
+  });
+  tasks.forEach((task) => {
+    if (task.task === currentTask) {
+      task.task = event.value;
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// SORT
+function sortList() {
+  let list, i, switching, b, shouldSwitch;
+  list = document.getElementById("ul-list");
+  switching = true;
+  while (switching) {
+    switching = false;
+    b = list.getElementsByTagName("LI");
+    for (i = 0; i < b.length - 1; i++) {
+      shouldSwitch = false;
+      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+}
